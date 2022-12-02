e85580c33482005f763776000d33e3b7c1ad187f
sehgithubtn <96432279+sehgithubtn@users.noreply.github.com>
2022-12-02 22:54:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,14 +38,19 @@ const __dirname = path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
-    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
-}else{
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname,'/frontend/build')))
+//     app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+// }else{
+//     app.get('/' , (req,res) => {
+//         res.send('API is running...')
+//     })
+// }
+
+
     app.get('/' , (req,res) => {
         res.send('API is running...')
     })
-}
 
 
 
